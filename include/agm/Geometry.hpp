2d9812124c21810208400f4389690900ba4ece68
Fatih Yazıcı <fatihyazici67@gmail.com>
2022-09-11 23:01:32 +0200

    updated scene creation

--- include/agm/Geometry.hpp
+++ include/agm/Geometry.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <array>
+#include <memory>
 
 #include "Vector.hpp"
 #include "Material.hpp"
@@ -26,18 +26,35 @@ struct Sphere: public Object<T>
     Material<> material;
 };
 
+
+template<typename T = double>
+struct InfinitePlane: public Object<T>
+{
+    InfinitePlane()
+    {
+    }
+
+    auto map(const Vec3<T> &p) const noexcept -> std::pair<T, Material<>> override
+    {
+        return { (p - pos).dot(normal), material };
+    }
+
+    Vec3<T> pos, normal;
+    Material<> material;
+};
+
 template<typename T = double>
 struct Union: public Object<T>
 {
-    Union(const Object<T> &obj1_, const Object<T> &obj2_) 
+    Union(const Object<T> * const obj1_, const Object<T> * const obj2_) 
         : obj1{obj1_}, obj2{obj2_}
     {
     }
 
     auto map(const Vec3<T> &p) const noexcept -> std::pair<T, Material<>> override
     {
-        auto d1m1 = obj1.map(p);
-        auto d2m2 = obj2.map(p);
+        auto d1m1 = obj1->map(p);
+        auto d2m2 = obj2->map(p);
 
         if (d1m1.first < d2m2.first)
             return d1m1;
@@ -45,21 +62,22 @@ struct Union: public Object<T>
             return d2m2;
     }
 
-    const Object<T> &obj1, &obj2;
+    const Object<T> * const obj1;
+    const Object<T> * const obj2;
 };
 
 template<typename T = double>
 struct Difference: public Object<T>
 {
-    Difference(const Object<T> &obj1_, const Object<T> &obj2_) 
+    Difference(const Object<T> * const obj1_, const Object<T> * const obj2_) 
         : obj1{obj1_}, obj2{obj2_}
     {
     }
 
     auto map(const Vec3<T> &p) const noexcept -> std::pair<T, Material<>> override
     {
-        auto [d1, m1] = obj1.map(p);
-        auto [d2, _] = obj2.map(p);
+        auto [d1, m1] = obj1->map(p);
+        auto [d2, m2] = obj2->map(p);
 
         if (d1 > -d2)
             return {d1, m1};
@@ -67,25 +85,26 @@ struct Difference: public Object<T>
             return {-d2, m1};
     }
 
-    const Object<T> &obj1, &obj2;
+    const Object<T> * const obj1;
+    const Object<T> * const obj2;
 };
 
 template<typename DistortFn, typename T = double>
 struct Distort: public Object<T>
 {
-    Distort(const Object<T> &obj_, DistortFn &&distort_fn_) 
+    Distort(const Object<T> * const obj_, DistortFn distort_fn_) 
         : obj{obj_}, distort_fn{distort_fn_}
     {
     }
 
     auto map(const Vec3<T> &p) const noexcept -> std::pair<T, Material<>> override
     {
-        auto [d, m] = obj.map(p);
+        auto [d, m] = obj->map(p);
         return { distort_fn(p, d), m };
     }
 
-    const Object<T> &obj;
-    const DistortFn &distort_fn;
+    const Object<T> * const obj;
+    DistortFn distort_fn;
 };
 
 } // namespace geometry
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,52 @@
 #include <fmt/core.h>
+#include <memory>
 
-//#define AGM_USE_OPENMP
+#define AGM_USE_OPENMP
 #include <agm/agm.h>
+
+auto create_scene(agm::ObjectFactory &factory) -> agm::Object<> *
+{
+    auto sphere1 = factory.make<agm::geometry::Sphere<>>();
+    {
+        sphere1->center = { 0.5, 0, 1 };
+        sphere1->radius = 1;
+        sphere1->material.ambient = {0.0, 0.0, 0.0};
+        sphere1->material.diffuse = {1.0, 0.0, 0.0};
+        sphere1->material.specular = {1.0, 0.8, 0.8};
+    }
+
+    auto sphere2 = factory.make<agm::geometry::Sphere<>>();
+    {
+        sphere2->center = { -0.2, 0, 0.7 };
+        sphere2->radius = 1;
+        sphere2->material.ambient = {0.05, 0.05, 0.05};
+        sphere2->material.diffuse = {0.0, 0.0, 1.0};
+        sphere2->material.specular = {1.0, 1.0, 1.0};
+    }
+
+    //auto union1 = factory.make<agm::geometry::Union<>>(sphere1, sphere2);
+
+    auto wiggle_fn = [](agm::Vec3<double> p, double d) { 
+        return d + 0.05 * std::sin(20 * p.x) * std::sin(20 * p.y) * std::sin(20 * p.z); 
+    };
+
+    auto distort1 = factory.make<agm::geometry::Distort<decltype(wiggle_fn)>>(sphere1, wiggle_fn);
+
+    auto difference1 = factory.make<agm::geometry::Difference<>>(distort1, sphere2);
+
+    auto plane1 = factory.make<agm::geometry::InfinitePlane<>>();
+    {
+        plane1->pos = { 0, 0, 0 };
+        plane1->normal = agm::Vec3<>{ 0, 1, 0 }.normalize();
+        plane1->material.ambient = {0.05, 0.05, 0.05};
+        plane1->material.diffuse = {0.0, 1.0, 0.0};
+        plane1->material.specular = {0.2, 0.2, 0.2};
+    }
+
+    auto union2 = factory.make<agm::geometry::Union<>>(difference1, plane1);
+    
+    return union2;
+}
 
 int main(int argc, char *argv[])
 {
@@ -17,47 +62,26 @@ int main(int argc, char *argv[])
     fmt::print("Setting up camera\n");
     agm::Camera camera1;
     {
-        camera1.position = {0, 0, -1};
-        camera1.left = {-1, 0, 0};
-        camera1.up = {0, 1, 0};
+        camera1.position = {0, 1, -1};
         camera1.gaze = {0, 0, 1};
-        camera1.focal_distance = 1;
-        camera1.maximum_distance = 100;
+        camera1.up = {0, 1, 0};
+        camera1.right = {1, 0, 0};
+        camera1.rotate_x(-30 * M_PI / 180);
+
+        camera1.near = 1;
+        camera1.far = 1000;
+        camera1.viewport = { -1, 1, 1, -1 };
     }
     
     fmt::print("Setting up scene\n");
-    agm::geometry::Sphere sphere1;
-    {
-        sphere1.center = { 0, 0, 1 };
-        sphere1.radius = 1;
-        sphere1.material.ambient = {0.05, 0.05, 0.05};
-        sphere1.material.diffuse = {1.0, 0.0, 0.0};
-        sphere1.material.specular = {1.0, 0.8, 0.8};
-    }
 
-    agm::geometry::Sphere sphere2;
-    {
-        sphere2.center = { -0.5, 0, 0.4 };
-        sphere2.radius = 0.5;
-        sphere2.material.ambient = {0.05, 0.05, 0.05};
-        sphere2.material.diffuse = {0.0, 1.0, 0.0};
-        sphere2.material.specular = {1.0, 1.0, 1.0};
-    }
-
-    auto union1 = agm::geometry::Union(sphere1, sphere2);
-    auto difference1 = agm::geometry::Difference(sphere1, sphere2);
-    auto distort1 = agm::geometry::Distort(difference1,
-        [](auto p, auto d) { 
-            return d + 0.05 * std::sin(10 * p.x) * std::sin(10 * p.y) * std::sin(10 * p.z); 
-        }
-    );
-
-    auto scene1 = distort1;
+    agm::ObjectFactory factory1;
+    auto scene1 = create_scene(factory1);
 
     fmt::print("Setting up light source\n");
     agm::Light light1;
     {
-        light1.position = {1, 1, -1};
+        light1.position = {2, 2, 0};
         light1.color = {0.96, 0.91, 0.60};
     }
 
@@ -67,7 +91,7 @@ int main(int argc, char *argv[])
     }
 
     fmt::print("Rendering scene\n");
-    agm::render(camera1, scene1, light1, {0, 0, 0}, surface1);
+    agm::render(camera1, *scene1, light1, {0, 0, 0}, surface1);
 
     fmt::print("Saving surface to file\n");
     agm::surface_to_file(surface1, output_filename);
