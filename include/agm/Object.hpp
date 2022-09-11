c4c020c5bb916096fc2790726d4a02e86b453d68
Fatih Yazıcı <fatihyazici67@gmail.com>
2022-09-11 22:59:28 +0200

    object factory for scene creation

--- include/agm/Object.hpp
+++ include/agm/Object.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #include "Vector.hpp"
 #include "Material.hpp"
 
@@ -10,6 +12,26 @@ template<typename T = double>
 struct Object
 {
     virtual auto map(const Vec3<T> &) const noexcept -> std::pair<T, Material<>> = 0;
+    virtual ~Object() = default;
+};
+
+struct ObjectFactory
+{
+    std::vector<agm::Object<> *> storage;
+
+    template<typename T, typename ...Args>
+    auto make(Args ...args) -> T *
+    {
+        auto obj = new T(std::forward<Args>(args)...);
+        storage.emplace_back(obj);
+        return obj;
+    }
+
+    virtual ~ObjectFactory()
+    {
+        for (auto obj: storage)
+            delete obj;
+    }
 };
 
 } // namespace agm
